5cbd1232a1b5597650c6af7a6163fa4fa2f369f9
raihan-muhammad <raihanmuhammad.dev@gmail.com>
2021-01-19 10:36:13 +0700

    update: added enum and type for action and actionType

--- src/redux/reducers/RepositoriesReducer.ts
+++ src/redux/reducers/RepositoriesReducer.ts
@@ -5,32 +5,40 @@ interface RepositoriesState {
 }
 
 interface SearchRepositories {
-  type: 'search_repositories';
+  type: ActionType.SEARCH_REPOSITORIES;
 }
 
 interface SearchRepositoriesSucess {
-  type: 'search_repositories_success';
+  type: ActionType.SEARCH_REPOSITORIES_SUCCESS;
   payload: string[];
 }
 
 interface SearchRepositoriesError {
-  type: 'search_repositories_error';
+  type: ActionType.SEARCH_REPOSITORIES_ERROR;
   payload: string;
+}
+
+type Action =
+  | SearchRepositories
+  | SearchRepositoriesSucess
+  | SearchRepositoriesError;
+
+enum ActionType {
+  SEARCH_REPOSITORIES = 'search_repositories',
+  SEARCH_REPOSITORIES_SUCCESS = 'search_repositories_success',
+  SEARCH_REPOSITORIES_ERROR = 'search_repositories_error',
 }
 
 const reducer = (
   state: RepositoriesState,
-  action:
-    | SearchRepositories
-    | SearchRepositoriesSucess
-    | SearchRepositoriesError
+  action: Action
 ): RepositoriesState => {
   switch (action.type) {
-    case 'search_repositories':
+    case ActionType.SEARCH_REPOSITORIES:
       return { loading: true, data: [], error: null };
-    case 'search_repositories_success':
+    case ActionType.SEARCH_REPOSITORIES_SUCCESS:
       return { loading: false, data: action.payload, error: null };
-    case 'search_repositories_error':
+    case ActionType.SEARCH_REPOSITORIES_ERROR:
       return { loading: false, data: [], error: action.payload };
     default:
       return state;
