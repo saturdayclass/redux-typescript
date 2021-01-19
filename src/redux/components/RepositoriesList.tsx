8960cb2807d6d884128042f29bfb309d44b8c1ea
raihan-muhammad <raihanmuhammad.dev@gmail.com>
2021-01-19 20:54:33 +0700

    update: create use type selector

--- src/redux/components/RepositoriesList.tsx
+++ src/redux/components/RepositoriesList.tsx
@@ -1,8 +1,12 @@
 import { useState } from 'react';
 import { useActions } from '../hooks/useActions';
+import { useTypedSelector } from '../hooks/useTypedSelector';
 const RepositoriesList: React.FC = () => {
   const [term, setTerm] = useState('');
   const { searchRepositories } = useActions();
+  const { data, loading, error } = useTypedSelector(
+    (state) => state.repositories
+  );
 
   const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
