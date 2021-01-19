22cd9fd5768370941c58a9b5c463ec1832e80605
raihan-muhammad <raihanmuhammad.dev@gmail.com>
2021-01-19 20:18:55 +0700

    update: added dispatch in repositoriesList

--- src/redux/components/RepositoriesList.tsx
+++ src/redux/components/RepositoriesList.tsx
@@ -1,9 +1,14 @@
 import { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { actionCreators } from '../index';
 const RepositoriesList: React.FC = () => {
+  const dispatch = useDispatch();
   const [term, setTerm] = useState('');
 
   const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
+
+    dispatch(actionCreators.searchRespositories(term));
   };
 
   return (
