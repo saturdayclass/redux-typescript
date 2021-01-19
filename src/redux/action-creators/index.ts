d6f1a1088055be27d774a777d087d6d7fc694a91
raihan-muhammad <raihanmuhammad.dev@gmail.com>
2021-01-19 21:02:26 +0700

    update: comsumeing store state

--- src/redux/action-creators/index.ts
+++ src/redux/action-creators/index.ts
@@ -15,7 +15,7 @@ export const searchRepositories = (term: string) => {
       );
 
       const names = data.objects.map((result: any) => {
-        return result.pacakge.name;
+        return result.package.name;
       });
 
       dispatch({
--- src/redux/components/RepositoriesList.tsx
+++ src/redux/components/RepositoriesList.tsx
@@ -21,6 +21,9 @@ const RepositoriesList: React.FC = () => {
         onChange={(e) => setTerm(e.target.value)}
       />
       <button type="submit">Search</button>
+      {error && <h3>{error}</h3>}
+      {loading && <h3>Loading....</h3>}
+      {!error && !loading && data.map((name) => <div key={name}>{name}</div>)}
     </form>
   );
 };
